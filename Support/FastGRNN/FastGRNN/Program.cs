a2d3369df81214933bcfd6db45952391b0637376
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-08-22 15:12:15 -0400

    Minor fix: had desabled the linear algebra packages by accident

--- Support/FastGRNN/FastGRNN/Program.cs
+++ Support/FastGRNN/FastGRNN/Program.cs
@@ -6,8 +6,8 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
-//using MathNet.Numerics;
-//using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
 
 class fastgrnn
 {
