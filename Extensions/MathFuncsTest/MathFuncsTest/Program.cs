6a536f681079b1009e1e196cd6e0ffd7d6ffe23a
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-08-25 19:09:34 -0400

    Better, cleaner test app

--- Extensions/MathFuncsTest/MathFuncsTest/Program.cs
+++ Extensions/MathFuncsTest/MathFuncsTest/Program.cs
@@ -8,230 +8,214 @@ namespace MathFuncsTest
     {
         public static void Main()
         {
-            const float largeFactor = 20.0f; // Suitably large factor, for downscaling. The larger this factor, the better the chance of
-                                             // matrix multiplication not causing Q31 overflow, at the cost of precision
 
+            /*Conversion between float and Q31*/
             float fl1 = 0.18f;
             int int1 = 25;
 
             Debug.Print(fl1 + " to Q31 = " + Support.ConvertFloatToQ31(fl1) + " and back to float = " + Support.ConvertQ31ToFloat(Support.ConvertFloatToQ31(fl1)));
             Debug.Print(int1 + " to float = " + Support.ConvertQ31ToFloat(int1) + " and back to int = " + Support.ConvertFloatToQ31(Support.ConvertQ31ToFloat(int1)));
 
-            float[] floatarr1 = new float[] { 1.0f/largeFactor, 2.0f/largeFactor, 3.0f/largeFactor, 4.0f/largeFactor, 5.0f/largeFactor, 6.0f/largeFactor };
-            int[] q31arr1 = new int[6];
-            float[] floatbackarr1 = new float[6];
 
-            Support.ConvertFloatToQ31(floatarr1, q31arr1);
+            /*Matrix initialization*/
+            float[] floatarr1 = new float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f };
+            float[] floatarr2 = new float[] { 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f };
+            float[] floatarr3 = new float[] { 8.0f, 7.0f, 6.0f, 5.0f, 4.0f, 3.0f };
 
+            Matrix M1 = new Matrix(2, 3, floatarr1);
+            Matrix M2 = new Matrix(3, 2, floatarr2);
+            Matrix M3 = new Matrix(2, 3, floatarr3);
+
+            /*Matrix multiplication*/
             Debug.Print("############");
             Debug.Print("# MATRIX 1 #");
             Debug.Print("############");
-            
-            Debug.Print("Scaled float array: ");
-            foreach (var item in floatarr1)
+
+            Debug.Print("Rows: " + M1.rows + "; cols: " + M1.cols + "; data:");
+            foreach (var item in M1.data)
             {
                 Debug.Print(item.ToString());
             }
-
-            Debug.Print("Converted Q31 array: ");
-            foreach (var item in q31arr1)
-            {
-                Debug.Print(item.ToString());
-            }
-
-            Support.ConvertQ31ToFloat(q31arr1, floatbackarr1);
-
-            Debug.Print("Converted back to float array: ");
-            foreach (var item in floatbackarr1)
-            {
-                Debug.Print(item.ToString());
-            }
-
-            float[] floatarr2 = new float[] { 7.0f/largeFactor, 8.0f/largeFactor, 9.0f/largeFactor, 10.0f/largeFactor, 11.0f/largeFactor, 12.0f/largeFactor };
-            int[] q31arr2 = new int[6];
-
-            Support.ConvertFloatToQ31(floatarr2, q31arr2);
 
             Debug.Print("############");
             Debug.Print("# MATRIX 2 #");
             Debug.Print("############");
 
-            Debug.Print("Scaled float array: ");
-            foreach (var item in floatarr2)
+            Debug.Print("Rows: " + M2.rows + "; cols: " + M2.cols + "; data:");
+            foreach (var item in M2.data)
             {
                 Debug.Print(item.ToString());
             }
 
-            Debug.Print("Converted Q31 array: ");
-            foreach (var item in q31arr2)
-            {
-                Debug.Print(item.ToString());
-            }
-
-            Matrix M1 = new Matrix(2, 3, q31arr1);
-            Matrix M2 = new Matrix(3, 2, q31arr2);
-            Matrix outmat;
-            MatrixFuncs.MatrixMult(M1, M2, out outmat);
-
-            float[] outmatfloat = new float[outmat.rows*outmat.cols];
-            Support.ConvertQ31ToFloat(outmat.data, outmatfloat);
+            Matrix outmat = MatrixFuncs.MatrixMult(M1, M2);
 
             Debug.Print("################################");
             Debug.Print("# MATRIX MULTIPLICATION OUTPUT #");
             Debug.Print("################################");
 
             Debug.Print("Out rows: " + outmat.rows + "; cols: " + outmat.cols + "; data:");
-            foreach (float item in outmatfloat)
+            foreach (float item in outmat.data)
             {
-                Debug.Print((item * largeFactor * largeFactor).ToString());
+                Debug.Print(item.ToString());
             }
 
+            /*Matrix addition*/
             Debug.Print("############");
             Debug.Print("# MATRIX 1 #");
             Debug.Print("############");
 
-            Debug.Print("Scaled float array: ");
-            foreach (var item in floatarr1)
+            Debug.Print("Rows: " + M1.rows + "; cols: " + M1.cols + "; data:");
+            foreach (var item in M1.data)
             {
                 Debug.Print(item.ToString());
             }
-
-            Debug.Print("Converted Q31 array: ");
-            foreach (var item in q31arr1)
-            {
-                Debug.Print(item.ToString());
-            }
-
-            float[] floatarr3 = new float[] { 8.0f / largeFactor, 7.0f / largeFactor, 6.0f / largeFactor, 5.0f / largeFactor, 4.0f / largeFactor, 3.0f / largeFactor };
-            int[] q31arr3 = new int[6];
-
-            Support.ConvertFloatToQ31(floatarr3, q31arr3);
 
             Debug.Print("############");
             Debug.Print("# MATRIX 3 #");
             Debug.Print("############");
 
-            Debug.Print("Scaled float array: ");
-            foreach (var item in floatarr3)
+            Debug.Print("Rows: " + M3.rows + "; cols: " + M3.cols + "; data:");
+            foreach (var item in M3.data)
             {
                 Debug.Print(item.ToString());
             }
 
-            Debug.Print("Converted Q31 array: ");
-            foreach (var item in q31arr3)
-            {
-                Debug.Print(item.ToString());
-            }
-
-            Matrix M3 = new Matrix(2, 3, q31arr3);
-
-            MatrixFuncs.MatrixAdd(M1, M3, out outmat);
-            outmatfloat = new float[outmat.rows * outmat.cols];
-            Support.ConvertQ31ToFloat(outmat.data, outmatfloat);
+            outmat = MatrixFuncs.MatrixAdd(M1, M3);
 
             Debug.Print("##########################");
             Debug.Print("# MATRIX ADDITION OUTPUT #");
             Debug.Print("##########################");
 
             Debug.Print("Out rows: " + outmat.rows + "; cols: " + outmat.cols + "; data:");
-            foreach (float item in outmatfloat)
+            foreach (float item in outmat.data)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
             }
 
-            MatrixFuncs.MatrixSub(M1, M3, out outmat);
-            outmatfloat = new float[outmat.rows * outmat.cols];
-            Support.ConvertQ31ToFloat(outmat.data, outmatfloat);
+            /*Matrix subtraction*/
+            outmat = MatrixFuncs.MatrixSub(M1, M3);
 
             Debug.Print("#############################");
             Debug.Print("# MATRIX SUBTRACTION OUTPUT #");
             Debug.Print("#############################");
 
             Debug.Print("Out rows: " + outmat.rows + "; cols: " + outmat.cols + "; data:");
-            foreach (float item in outmatfloat)
+            foreach (float item in outmat.data)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
+            }
+
+            /*Matrix transpose*/
+            Debug.Print("############");
+            Debug.Print("# MATRIX 1 #");
+            Debug.Print("############");
+
+            Debug.Print("Rows: " + M1.rows + "; cols: " + M1.cols + "; data:");
+            foreach (var item in M1.data)
+            {
+                Debug.Print(item.ToString());
             }
 
             Debug.Print("######################");
             Debug.Print("# MATRIX 1 TRANSPOSE #");
             Debug.Print("######################");
 
-            MatrixFuncs.MatrixTrans(M1, out outmat);
-            outmatfloat = new float[outmat.rows * outmat.cols];
-            Support.ConvertQ31ToFloat(outmat.data, outmatfloat);
+            outmat = MatrixFuncs.MatrixTrans(M1);
 
             Debug.Print("Out rows: " + outmat.rows + "; cols: " + outmat.cols + "; data:");
-            foreach (float item in outmatfloat)
+            foreach (float item in outmat.data)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
+            }
+
+            Debug.Print("############");
+            Debug.Print("# MATRIX 2 #");
+            Debug.Print("############");
+
+            Debug.Print("Rows: " + M2.rows + "; cols: " + M2.cols + "; data:");
+            foreach (var item in M2.data)
+            {
+                Debug.Print(item.ToString());
             }
 
             Debug.Print("######################");
             Debug.Print("# MATRIX 2 TRANSPOSE #");
             Debug.Print("######################");
 
-            MatrixFuncs.MatrixTrans(M2, out outmat);
-            outmatfloat = new float[outmat.rows * outmat.cols];
-            Support.ConvertQ31ToFloat(outmat.data, outmatfloat);
+            outmat = MatrixFuncs.MatrixTrans(M2);
 
             Debug.Print("Out rows: " + outmat.rows + "; cols: " + outmat.cols + "; data:");
-            foreach (float item in outmatfloat)
+            foreach (float item in outmat.data)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
+            }
+
+            Debug.Print("############");
+            Debug.Print("# MATRIX 3 #");
+            Debug.Print("############");
+
+            Debug.Print("Rows: " + M3.rows + "; cols: " + M3.cols + "; data:");
+            foreach (var item in M3.data)
+            {
+                Debug.Print(item.ToString());
             }
 
             Debug.Print("######################");
-            Debug.Print("# MATRIX 3 TRANSPOSE #");
+            Debug.Print("# MATRIX 1 TRANSPOSE #");
             Debug.Print("######################");
 
-            MatrixFuncs.MatrixTrans(M3, out outmat);
-            outmatfloat = new float[outmat.rows * outmat.cols];
-            Support.ConvertQ31ToFloat(outmat.data, outmatfloat);
+            outmat = MatrixFuncs.MatrixTrans(M3);
 
             Debug.Print("Out rows: " + outmat.rows + "; cols: " + outmat.cols + "; data:");
-            foreach (float item in outmatfloat)
+            foreach (float item in outmat.data)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
             }
 
-
-            int[] q31arr1copy = new int[q31arr1.Length];
-            Support.VectorCopy(q31arr1, q31arr1copy);
+            /*Vector copy*/
             Debug.Print("############");
             Debug.Print("# VECTOR 1 #");
             Debug.Print("############");
 
-            Debug.Print("Scaled float array: ");
+            Debug.Print("Data:");
             foreach (var item in floatarr1)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
+
             }
 
-            float[] floatarr1copy = new float[q31arr1copy.Length];
-            Support.ConvertQ31ToFloat(q31arr1copy, floatarr1copy);
-            Debug.Print("Copy: ");
-            foreach (var item in floatarr1copy)
+            float[] copyarr1 = Support.VectorCopy(floatarr1);
+
+            Debug.Print("#################");
+            Debug.Print("# VECTOR 1 COPY #");
+            Debug.Print("#################");
+
+            Debug.Print("Data:");
+            foreach (var item in copyarr1)
             {
-                Debug.Print((item*largeFactor).ToString());
+                Debug.Print(item.ToString());
+
             }
 
-            float scalar = 3.5f;
-            int[] fillVec = new int[6];
-            Support.VectorFill(Support.ConvertFloatToQ31(scalar / largeFactor), fillVec);
+            float scal1 = 3.5f;
 
-            Debug.Print("##################");
-            Debug.Print("# FILLING VECTOR #");
-            Debug.Print("##################");
-            Debug.Print("Scalar: " + scalar);
+            /*Vector fill with scalar */
+            Debug.Print("############");
+            Debug.Print("# SCALAR 1 #");
+            Debug.Print("############");
 
-            float[] fillVecFloat = new float[fillVec.Length];
-            Support.ConvertQ31ToFloat(fillVec, fillVecFloat);
-            
-            Debug.Print("Filled vector: ");
-            foreach (var item in fillVecFloat)
+            Debug.Print("Value:" +scal1 );
+
+            float[] fillarr1 = Support.VectorFill(6, scal1);
+            Debug.Print("#############################");
+            Debug.Print("# VECTOR FILL WITH SCALAR 1 #");
+            Debug.Print("#############################");
+
+            Debug.Print("Vector length: " + fillarr1.Length + "; data:");
+            foreach (var item in fillarr1)
             {
-                Debug.Print((item * largeFactor).ToString());
+                Debug.Print(item.ToString());
+
             }
         }
     }
