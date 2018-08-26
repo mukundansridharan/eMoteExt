7f155de4ab116638c424e8d50e854ebef19e1202
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-08-25 20:51:02 -0400

    Compiling stubs for vector functions

--- Extensions/MathFuncs/Class1.cs
+++ Extensions/MathFuncs/Class1.cs
@@ -66,6 +66,7 @@ namespace CMSIS
             return outx;
         }
 
+        // Deep-copy one vector into another
         public static float[] VectorCopy(float[] invec)
         {
             int[] outvec = new int[invec.Length];
@@ -73,12 +74,14 @@ namespace CMSIS
             return ScaleConvertQ31ArrToFloat(outvec, GlobalVar.largeFactor);
         }
 
+        // Fill vector with scalar
         public static float[] VectorFill(int len, float val)
         {
             int[] outvec = new int[len];
             VectorFill_Nat(ScaleConvertFloatToQ31(val, GlobalVar.largeFactor), outvec);
             return ScaleConvertQ31ArrToFloat(outvec, GlobalVar.largeFactor);
         }
+
 
         // Natives
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
@@ -103,7 +106,7 @@ namespace CMSIS
     public class MatrixFuncs
     {
 
-        // Wrappers
+        // Multiply two matrices
         public static Matrix MatrixMult(Matrix m1, Matrix m2)
         {
             int[] m1dat = Support.ScaleConvertFloatArrToQ31(m1.data, GlobalVar.largeFactor);
@@ -116,6 +119,7 @@ namespace CMSIS
             return outmat;
         }
 
+        // Add two matrices
         public static Matrix MatrixAdd(Matrix m1, Matrix m2)
         {
             int[] m1dat = Support.ScaleConvertFloatArrToQ31(m1.data, GlobalVar.largeFactor);
@@ -128,6 +132,7 @@ namespace CMSIS
             return outmat;
         }
 
+        // Subtract one matrix from another
         public static Matrix MatrixSub(Matrix m1, Matrix m2)
         {
             int[] m1dat = Support.ScaleConvertFloatArrToQ31(m1.data, GlobalVar.largeFactor);
@@ -140,6 +145,7 @@ namespace CMSIS
             return outmat;
         }
 
+        // Transpose a matrix
         public static Matrix MatrixTrans(Matrix m1)
         {
             int[] m1dat = Support.ScaleConvertFloatArrToQ31(m1.data, GlobalVar.largeFactor);
@@ -171,5 +177,104 @@ namespace CMSIS
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         extern static void MatrixTrans_Nat(UInt16 m1_rows, UInt16 m1_cols, int[] m1_data,
             out UInt16 outrows, out UInt16 outcols, int[] outdata);
+    }
+
+    public class VectorFuncs
+    {
+        // Find absolute of vector
+        public static float[] VectorAbs(float[] invec)
+        {
+            int[] indata = Support.ScaleConvertFloatArrToQ31(invec, GlobalVar.largeFactor);
+            int[] outdata = new int[invec.Length];
+
+            VectorAbs_Nat(indata, outdata);
+            return Support.ScaleConvertQ31ArrToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+        // Negate Vector
+        public static float[] VectorNegate(float[] invec)
+        {
+            int[] indata = Support.ScaleConvertFloatArrToQ31(invec, GlobalVar.largeFactor);
+            int[] outdata = new int[invec.Length];
+
+            VectorNegate_Nat(indata, outdata);
+            return Support.ScaleConvertQ31ArrToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+        // Add an offset to vector
+        public static float[] VectorOffset(float[] invec, float offset)
+        {
+            int[] indata = Support.ScaleConvertFloatArrToQ31(invec, GlobalVar.largeFactor);
+            int[] outdata = new int[invec.Length];
+
+            VectorOffset_Nat(indata, Support.ScaleConvertFloatToQ31(offset, GlobalVar.largeFactor), outdata);
+            return Support.ScaleConvertQ31ArrToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+        // Add two vectors
+        public static float[] VectorAdd(float[] in1vec, float[] in2vec)
+        {
+            int[] in1data = Support.ScaleConvertFloatArrToQ31(in1vec, GlobalVar.largeFactor);
+            int[] in2data = Support.ScaleConvertFloatArrToQ31(in2vec, GlobalVar.largeFactor);
+            int[] outdata = new int[in1vec.Length];
+
+            VectorAdd_Nat(in1data, in2data, outdata);
+            return Support.ScaleConvertQ31ArrToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+        // Subtract one vector from another
+        public static float[] VectorSub(float[] in1vec, float[] in2vec)
+        {
+            int[] in1data = Support.ScaleConvertFloatArrToQ31(in1vec, GlobalVar.largeFactor);
+            int[] in2data = Support.ScaleConvertFloatArrToQ31(in2vec, GlobalVar.largeFactor);
+            int[] outdata = new int[in1vec.Length];
+
+            VectorSub_Nat(in1data, in2data, outdata);
+            return Support.ScaleConvertQ31ArrToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+        // Dot product of two vectors
+        public static float VectorDot(float[] in1vec, float[] in2vec)
+        {
+            int[] in1data = Support.ScaleConvertFloatArrToQ31(in1vec, GlobalVar.largeFactor);
+            int[] in2data = Support.ScaleConvertFloatArrToQ31(in2vec, GlobalVar.largeFactor);
+            
+            int outdata = VectorDot_Nat(in1data, in2data);
+            return Support.ScaleConvertQ31ToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+        // Hadamard product of two vectors
+        public static float[] VectorHadamad(float[] in1vec, float[] in2vec)
+        {
+            int[] in1data = Support.ScaleConvertFloatArrToQ31(in1vec, GlobalVar.largeFactor);
+            int[] in2data = Support.ScaleConvertFloatArrToQ31(in2vec, GlobalVar.largeFactor);
+            int[] outdata = new int[in1vec.Length];
+
+            VectorHadamard_Nat(in1data, in2data, outdata);
+            return Support.ScaleConvertQ31ArrToFloat(outdata, GlobalVar.largeFactor);
+        }
+
+
+        // Natives
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void VectorAbs_Nat(int[] intx, int[] outx);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void VectorNegate_Nat(int[] intx, int[] outx);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void VectorOffset_Nat(int[] intx, int offset, int[] outx);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void VectorAdd_Nat(int[] intx, int[] inty, int[] outx);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void VectorSub_Nat(int[] intx, int[] inty, int[] outx);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static int VectorDot_Nat(int[] intx, int[] inty);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void VectorHadamard_Nat(int[] intx, int[] inty, int[] outx);
     }
 }
--- Extensions/MathFuncs/Stubs/MathFuncs.cpp
+++ Extensions/MathFuncs/Stubs/MathFuncs.cpp
@@ -35,12 +35,27 @@ static const CLR_RT_MethodHandler method_lookup[] =
     Library_MathFuncs_CMSIS_Support::ConvertQ31ToFloat___STATIC__VOID__SZARRAY_I4__SZARRAY_R4,
     Library_MathFuncs_CMSIS_Support::VectorCopy_Nat___STATIC__VOID__SZARRAY_I4__SZARRAY_I4,
     Library_MathFuncs_CMSIS_Support::VectorFill_Nat___STATIC__VOID__I4__SZARRAY_I4,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorAbs_Nat___STATIC__VOID__SZARRAY_I4__SZARRAY_I4,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorNegate_Nat___STATIC__VOID__SZARRAY_I4__SZARRAY_I4,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorOffset_Nat___STATIC__VOID__SZARRAY_I4__I4__SZARRAY_I4,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorAdd_Nat___STATIC__VOID__SZARRAY_I4__SZARRAY_I4__SZARRAY_I4,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorSub_Nat___STATIC__VOID__SZARRAY_I4__SZARRAY_I4__SZARRAY_I4,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorDot_Nat___STATIC__I4__SZARRAY_I4__SZARRAY_I4,
+    Library_MathFuncs_CMSIS_VectorFuncs::VectorHadamard_Nat___STATIC__VOID__SZARRAY_I4__SZARRAY_I4__SZARRAY_I4,
 };
 
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_MathFuncs =
 {
     "MathFuncs", 
-    0x487C24E4,
+    0x9489DFF9,
     method_lookup
 };
 
