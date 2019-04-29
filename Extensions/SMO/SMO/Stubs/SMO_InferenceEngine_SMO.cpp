84d528c76c60cbe4dc3501f46b9d9e5d83535c11
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-04-29 15:23:32 -0400

    Adding stubs directory

---
+++ Extensions/SMO/SMO/Stubs/SMO_InferenceEngine_SMO.cpp
@@ -0,0 +1,29 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+
+#include "SMO.h"
+#include "SMO_InferenceEngine_SMO.h"
+#include "SMONative.cpp"
+
+using namespace InferenceEngine;
+
+float SMO::predictCount( CLR_RT_HeapBlock* pMngObj, CLR_RT_TypedArray_float param0, HRESULT &hr )
+{
+    return SMONative::predictCount(param0.GetBuffer());
+}
+
+double SMO::predictClass( CLR_RT_HeapBlock* pMngObj, CLR_RT_TypedArray_float param0, HRESULT &hr )
+{
+    return SMONative::predictClass(param0.GetBuffer());
+}
+
