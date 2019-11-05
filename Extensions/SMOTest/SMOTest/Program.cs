03ad5ef8a0b54e5a69b30cf4da9b86d3c8131792
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-11-05 12:12:32 -0500

    Adding upper tier to SMOTest

--- Extensions/SMOTest/SMOTest/Program.cs
+++ Extensions/SMOTest/SMOTest/Program.cs
@@ -9,12 +9,17 @@ namespace SMOTest
     {
         public static void Main()
         {
-            //Debug.Print("Connect Logic Analyzer. Sleeping for 5 seconds");
+            Debug.Print("Connect Logic Analyzer. Sleeping for 5 seconds");
             Thread.Sleep(5000);
-            SMO emitest = new SMO();
+            // EMI test
+            Debug.Print("Running lower tier test...");
+            EMI emitest = new EMI();
             emitest.runEMITest();
-            //ushort[] test_arr = new ushort[512];
-            //emitest.EMIDriver(test_arr);
+            Thread.Sleep(2000);
+            // FastGRNN test
+            Debug.Print("Running uppertier test...");
+            FastGRNN fgrnntest = new FastGRNN();
+            fgrnntest.runFastGRNNTest();
         }
     }
 }
